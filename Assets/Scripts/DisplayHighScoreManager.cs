fbe248f2938d9202b1ffd985072dff3fcf976a7c
mattsedd <mattsedd87@icloud.com>
2024-08-22 15:18:13 +0200

    new commit
    small changes

--- Assets/Scripts/DisplayHighScoreManager.cs
+++ Assets/Scripts/DisplayHighScoreManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DisplayHighScoreManager : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI highScore;
+    [SerializeField] private TextMeshProUGUI highScorePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
